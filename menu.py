81953c756c1c48f375e304869a6dd64da938e2ba
aramadan0096 <57462754+aramadan0096@users.noreply.github.com>
2022-12-23 09:00:29 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,6 +6,8 @@ n = toolbar.addMenu("BT_Tools")
 #Transform
 n.addCommand("Transform/BT PresPaint", "nuke.createNode(\"BT_PresPaint.gizmo\")")
 
+from Scripts import *
+import nkProject_setup
 
 RamadanMenu = nuke.menu( 'Nuke' ).addMenu( 'BT Menu' )
 RamadanMenu.addCommand('Processual Save', 'nkProject_setup.project_setup()','ctrl+alt+s')
